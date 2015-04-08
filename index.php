54a6395b95d3f7278a5d9c10f98b0b59a1ba3d5b
unknown <iluchten@I0218126.odl.vdab.net>
2015-04-08 14:12:03 +0200

    --all

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Mijn eerste PHP pagina</title>
+    </head>
+    <body>
+        <h1>Mijn eerste PHP pagina</h1>
+        <?php
+        print("Dit is mijn eerste PHP code!")
+        ?>
+    </body>
+</html>
